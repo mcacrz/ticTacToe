78900a6022b52a243013672e6c30b877ee7254f9
marcelo <marcelo@sincronica.com.br>
2024-08-02 18:08:15 -0300

    feat: versão 1.0.0 do jogo da velha (tic-tac-toe)

---
+++ base/ticTacToe.js
@@ -0,0 +1,115 @@
+export default function () {
+  
+  const newPlay = () => ({
+    p1:'',
+    p2:'',
+    p3:'',
+    p4:'',
+    p5:'',
+    p6:'',
+    p7:'',
+    p8:'',
+    p9:''
+  })
+
+  const setP1 = (table, symbol) => {
+    table.p1 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP2 = (table, symbol) => {
+    table.p2 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP3 = (table, symbol) => {
+    table.p3 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP4 = (table, symbol) => {
+    table.p4 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP5 = (table, symbol) => {
+    table.p5 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP6 = (table, symbol) => {
+    table.p6 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP7 = (table, symbol) => {
+    table.p7 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP8 = (table, symbol) => {
+    table.p8 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const setP9 = (table, symbol) => {
+    table.p9 = (symbol.toLowerCase() !== 'x' && symbol.toLowerCase() !== 'o') ? '' : symbol.toLowerCase();
+    return table;
+  }
+
+  const verifyDraw = (table) => {
+    return (table.p1.length > 0 && table.p2.length > 0 && table.p3.length> 0 && table.p4.length > 0 && table.p5.length > 0 && table.p6.length > 0 && table.p7.length > 0 && table.p8.length > 0 && table.p9.length > 0)
+  }
+
+  const isFinish = (table) => {
+    const ruleHTop = () => ((table.p1.length > 0 && table.p2.length > 0 && table.p3.length > 0) && (table.p1 === table.p2 && table.p2 === table.p3 && table.p3 === table.p1));
+    const ruleHMiddle = () => ((table.p4.length > 0 && table.p5.length > 0 && table.p6.length > 0) && (table.p4 === table.p5 && table.p5 === table.p6 && table.p6 === table.p4));
+    const ruleHBottom = () => ((table.p7.length > 0 && table.p8.length > 0 && table.p9.length > 0) && (table.p7 === table.p8 && table.p8 === table.p9 && table.p9 === table.p7));
+    const ruleVLeft = () => ((table.p1.length > 0 && table.p4.length > 0 && table.p7.length > 0) && (table.p1 === table.p4 && table.p4 === table.p7 && table.p7 === table.p1));
+    const ruleVMiddle = () => ((table.p2.length > 0 && table.p5.length > 0 && table.p8.length > 0) && (table.p2 === table.p5 && table.p5 === table.p8 && table.p8 === table.p2));
+    const ruleVRight = () => ((table.p3.length > 0 && table.p6.length > 0 && table.p9.length > 0) && (table.p3 === table.p6 && table.p6 === table.p9 && table.p9 === table.p3));
+    const ruleDLeftRight = () => ((table.p1.length > 0 && table.p5.length > 0 && table.p9.length > 0) && (table.p1 === table.p5 && table.p5 === table.p9 && table.p9 === table.p1));
+    const ruleDRightLeft = () => ((table.p3.length > 0 && table.p5.length > 0 && table.p7.length > 0) && (table.p3 === table.p5 && table.p5 === table.p7 && table.p7 === table.p3));
+
+    const finishMoves = {
+      [ruleHTop()] : () => ({status: true, rule: 'Horizontal Top'}),
+      [ruleHMiddle()]: () => ({status: true, rule: 'Horizontal Middle'}),
+      [ruleHBottom()]: () => ({status: true, rule: 'Horizontal Bottom'}),
+      [ruleVLeft()]: () => ({status: true, rule: 'Vertical Left'}),
+      [ruleVMiddle()]: () => ({status: true, rule: 'Vertical Middle'}),
+      [ruleVRight()]: () => ({status: true, rule: 'Vertical Right'}),
+      [ruleDLeftRight()]: () => ({status: true, rule: 'Diagonal Left Right'}),
+      [ruleDRightLeft()]: () => ({status: true, rule: 'Diagonal Right Left'})
+    }
+
+    if (finishMoves['true']) {
+      return finishMoves['true']();
+    } 
+    
+    return verifyDraw(table) ? {status: true, rule: 'Draw'} : false;
+  }
+
+  const move = (table, position, symbol) => {
+    if (table) {
+      const plays = {
+        'p1' : () => setP1(table, symbol),
+        'p2' : () => setP2(table, symbol),
+        'p3' : () => setP3(table, symbol),
+        'p4' : () => setP4(table, symbol),
+        'p5' : () => setP5(table, symbol),
+        'p6' : () => setP6(table, symbol),
+        'p7' : () => setP7(table, symbol),
+        'p8' : () => setP8(table, symbol),
+        'p9' : () => setP9(table, symbol),
+      };
+
+      if (table[position].length === 0) table = plays[position](table, symbol);
+
+      return table;
+    }
+    
+    return false;
+  }
+ 
+  return { newPlay, move, setP1, setP2, setP3, setP4, setP5, setP6, setP7, setP8, setP9, isFinish, verifyDraw }
+}
---
+++ test/ticTacToe.test.js
@@ -0,0 +1,499 @@
+import { ticTacToe } from '../base'
+describe('suit test for tic-tac-toe game', () => {
+  const game = ticTacToe();
+  test('should create an object with 9 positions', () => {
+    const table = game.newPlay();
+    expect(Object.keys(table).length).toBe(9);
+  });
+
+  test('should set p1 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP1(table,'x');
+
+    expect(table.p1).toBe('x');
+  })
+
+  test('should set p1 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP1(table,'x');
+
+    expect(table.p1).toBe('x');
+  })
+
+  test('should mantain p1 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP1(table,'y');
+
+    expect(table.p1).toBe('');
+  })
+
+  test('should set p2 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP2(table,'x');
+
+    expect(table.p2).toBe('x');
+  })
+
+  test('should set p2 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP2(table,'x');
+
+    expect(table.p2).toBe('x');
+  })
+
+  test('should mantain p2 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP2(table,'y');
+
+    expect(table.p2).toBe('');
+  })
+
+  test('should set p3 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP3(table,'x');
+
+    expect(table.p3).toBe('x');
+  })
+
+  test('should set p3 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP3(table,'x');
+
+    expect(table.p3).toBe('x');
+  })
+
+  test('should mantain p3 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP3(table,'y');
+
+    expect(table.p3).toBe('');
+  })
+
+  test('should set p4 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP4(table,'x');
+
+    expect(table.p4).toBe('x');
+  })
+
+  test('should set p4 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP4(table,'x');
+
+    expect(table.p4).toBe('x');
+  })
+
+  test('should mantain p4 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP4(table,'y');
+
+    expect(table.p4).toBe('');
+  })
+
+  test('should set p5 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP5(table,'x');
+
+    expect(table.p5).toBe('x');
+  })
+
+  test('should set p5 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP5(table,'x');
+
+    expect(table.p5).toBe('x');
+  })
+
+  test('should mantain p5 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP5(table,'y');
+
+    expect(table.p5).toBe('');
+  })
+
+  test('should set p6 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP6(table,'x');
+
+    expect(table.p6).toBe('x');
+  })
+
+  test('should set p6 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP6(table,'x');
+
+    expect(table.p6).toBe('x');
+  })
+
+  test('should mantain p6 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP6(table,'y');
+
+    expect(table.p6).toBe('');
+  })
+
+  test('should set p7 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP7(table,'x');
+
+    expect(table.p7).toBe('x');
+  })
+
+  test('should set p7 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP7(table,'x');
+
+    expect(table.p7).toBe('x');
+  })
+
+  test('should mantain p7 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP7(table,'y');
+
+    expect(table.p7).toBe('');
+  })
+
+  test('should set p8 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP8(table,'x');
+
+    expect(table.p8).toBe('x');
+  })
+
+  test('should set p8 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP8(table,'x');
+
+    expect(table.p8).toBe('x');
+  })
+
+  test('should mantain p8 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP8(table,'y');
+
+    expect(table.p8).toBe('');
+  })
+  
+  test('should set p9 position with "x" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP9(table,'x');
+
+    expect(table.p9).toBe('x');
+  })
+
+  test('should set p9 position with "o" symbol' , () => {
+    let table = game.newPlay();
+    table = game.setP9(table,'x');
+
+    expect(table.p9).toBe('x');
+  })
+
+  test('should mantain p9 position empty if symbol is diferent of "x" or "o"', () => {
+    let table = game.newPlay();
+    table = game.setP9(table,'y');
+
+    expect(table.p9).toBe('');
+  })
+
+  test('should return status true game if rule Horizontal Top is true', () => {
+    let table = game.newPlay();
+    table = game.setP1(table, 'x');
+    table = game.setP2(table, 'x');
+    table = game.setP3(table, 'x');
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Horizontal Top'});
+  })
+
+  test('should return status true if rule Horizontal Middle is true', () => {
+    let table = game.newPlay();
+    table = game.setP4(table, 'x');
+    table = game.setP5(table, 'x');
+    table = game.setP6(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Horizontal Middle'});
+  })
+
+  test('should return status true if rule Horizontal Bottom is true', () => {
+    let table = game.newPlay();
+    table = game.setP7(table, 'x');
+    table = game.setP8(table, 'x');
+    table = game.setP9(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Horizontal Bottom'});
+  })
+
+  test('should return status true if rule Vertical Left is true', () => {
+    let table = game.newPlay();
+    table = game.setP1(table, 'x');
+    table = game.setP4(table, 'x');
+    table = game.setP7(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Vertical Left'});
+  })
+
+  test('should return status true if rule Vertical Middle is true', () => {
+    let table = game.newPlay();
+    table = game.setP2(table, 'x');
+    table = game.setP5(table, 'x');
+    table = game.setP8(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Vertical Middle'});
+  })
+
+  test('should return status true if rule Vertical Right is true', () => {
+    let table = game.newPlay();
+    table = game.setP3(table, 'x');
+    table = game.setP6(table, 'x');
+    table = game.setP9(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Vertical Right'});
+  })
+
+  test('should return status true if rule Diagonal Left Right is true', () => {
+    let table = game.newPlay();
+    table = game.setP1(table, 'x');
+    table = game.setP5(table, 'x');
+    table = game.setP9(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Diagonal Left Right'});
+  })
+
+  test('should return status true if rule Diagonal Right Left is true', () => {
+    let table = game.newPlay();
+    table = game.setP3(table, 'x');
+    table = game.setP5(table, 'x');
+    table = game.setP7(table, 'x');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Diagonal Right Left'});
+  })
+
+  test('should return false if game is no draw', () => {
+    let table = game.newPlay();
+    table = game.setP1(table, 'x');
+    table = game.setP2(table, 'o');
+    table = game.setP3(table, 'x');
+    table = game.setP4(table, 'o');
+    table = game.setP5(table, 'x');
+    table = game.setP6(table, 'o');
+    table = game.setP7(table, 'x');
+    table = game.setP8(table, 'o');
+    table = game.setP9(table, 'x');
+    
+    expect(game.verifyDraw(table)).toBeTruthy();
+  });
+
+  test('should return true if game is draw', () => {
+    let table = game.newPlay();
+    table = game.setP1(table, 'x');
+    
+    expect(game.verifyDraw(table)).toBeFalsy();
+  });
+
+
+  test('should return a finish game with a draw', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p2', 'o');
+    table = game.move(table, 'p3', 'x');
+    table = game.move(table, 'p4', 'o');
+    table = game.move(table, 'p5', 'o');
+    table = game.move(table, 'p6', 'x');
+    table = game.move(table, 'p7', 'x');
+    table = game.move(table, 'p8', 'x');
+    table = game.move(table, 'p9', 'o');
+    
+    expect(game.isFinish(table)).toEqual({status:true, rule: 'Draw'});
+  });
+
+  test('should return a finish game with a Horizontal Top with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p7', 'o');
+    table = game.move(table, 'p2', 'x');
+    table = game.move(table, 'p8', 'o');
+    table = game.move(table, 'p3', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Horizontal Top'});
+  });
+
+  test('should return a finish game with a Horizontal Top with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p7', 'x');
+    table = game.move(table, 'p2', 'o');
+    table = game.move(table, 'p8', 'x');
+    table = game.move(table, 'p3', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Horizontal Top'});
+  });
+
+  test('should return a finish game with a Horizontal Middle with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p4', 'x');
+    table = game.move(table, 'p8', 'o');
+    table = game.move(table, 'p5', 'x');
+    table = game.move(table, 'p3', 'o');
+    table = game.move(table, 'p6', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Horizontal Middle'});
+  });
+
+  test('should return a finish game with a Horizontal Middle with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p4', 'o');
+    table = game.move(table, 'p8', 'x');
+    table = game.move(table, 'p5', 'o');
+    table = game.move(table, 'p3', 'x');
+    table = game.move(table, 'p6', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Horizontal Middle'});
+  });
+
+  test('should return a finish game with a Horizontal Bottom with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p7', 'x');
+    table = game.move(table, 'p5', 'o');
+    table = game.move(table, 'p8', 'x');
+    table = game.move(table, 'p3', 'o');
+    table = game.move(table, 'p9', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Horizontal Bottom'});
+  });
+
+  test('should return a finish game with a Horizontal Bottom with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p7', 'o');
+    table = game.move(table, 'p5', 'x');
+    table = game.move(table, 'p8', 'o');
+    table = game.move(table, 'p3', 'x');
+    table = game.move(table, 'p9', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Horizontal Bottom'});
+  });
+
+  test('should return a finish game with a Vertical Left with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p2', 'o');
+    table = game.move(table, 'p4', 'x');
+    table = game.move(table, 'p8', 'o');
+    table = game.move(table, 'p7', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Vertical Left'});
+  });
+
+  test('should return a finish game with a Vertical Left with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p2', 'x');
+    table = game.move(table, 'p4', 'o');
+    table = game.move(table, 'p8', 'x');
+    table = game.move(table, 'p7', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Vertical Left'});
+  });
+
+  test('should return a finish game with a Vertical Middle with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p2', 'x');
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p5', 'x');
+    table = game.move(table, 'p7', 'o');
+    table = game.move(table, 'p8', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Vertical Middle'});
+  });
+
+  test('should return a finish game with a Vertical Middle with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p2', 'o');
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p5', 'o');
+    table = game.move(table, 'p7', 'x');
+    table = game.move(table, 'p8', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Vertical Middle'});
+  });
+
+  test('should return a finish game with a Vertical Right with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p3', 'x');
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p6', 'x');
+    table = game.move(table, 'p7', 'o');
+    table = game.move(table, 'p9', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Vertical Right'});
+  });
+
+  test('should return a finish game with a Vertical Right with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p3', 'o');
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p6', 'o');
+    table = game.move(table, 'p7', 'x');
+    table = game.move(table, 'p9', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Vertical Right'});
+  });
+
+  test('should return a finish game with a Diagonal Left Right with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p3', 'o');
+    table = game.move(table, 'p5', 'x');
+    table = game.move(table, 'p6', 'o');
+    table = game.move(table, 'p9', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Diagonal Left Right'});
+  });
+
+  test('should return a finish game with a Diagonal Left Right with o', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p3', 'x');
+    table = game.move(table, 'p5', 'o');
+    table = game.move(table, 'p6', 'x');
+    table = game.move(table, 'p9', 'o');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Diagonal Left Right'});
+  });
+
+  test('should return a finish game with a Diagonal Right Left with x', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p3', 'x');
+    table = game.move(table, 'p1', 'o');
+    table = game.move(table, 'p5', 'x');
+    table = game.move(table, 'p4', 'o');
+    table = game.move(table, 'p7', 'x');
+      
+    expect(game.isFinish(table)).toEqual({status: true, rule: 'Diagonal Right Left'});
+  });
+
+  test('should return false if move use a symbol that is diferent of x or o  ', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p3', 'y');
+    table = game.move(table, 'p1', 'x');
+    table = game.move(table, 'p5', 'y');
+    table = game.move(table, 'p4', 'x');
+    table = game.move(table, 'p7', 'y');
+      
+    expect(game.isFinish(table)).toBeFalsy();
+  });
+
+  test('should return same table if a move is make in a field filled', () => {
+    let table = game.newPlay();
+    table = game.move(table, 'p1', 'x');
+      
+    expect(game.move(table, 'p1', 'o')).toEqual(table);
+  });
+
+  test('should return same table if a move is call without table parameter', () => {
+    let table = game.newPlay();
+          
+    expect(game.move(null, 'p1', 'o')).toBeFalsy();
+  });
+});
